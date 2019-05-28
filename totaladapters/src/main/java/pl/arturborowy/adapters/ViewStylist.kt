88e939522e5ae613a6e0245dcb305e5080793f1c
arturborowy <artur.borowy@proexe.pl>
2019-05-28 20:25:26 +0200

    Make ViewStylist internal.

--- totaladapters/src/main/java/pl/arturborowy/adapters/ViewStylist.kt
+++ totaladapters/src/main/java/pl/arturborowy/adapters/ViewStylist.kt
@@ -2,7 +2,7 @@ package pl.arturborowy.adapters
 
 import android.view.View
 
-interface ViewStylist<ViewT : View, ItemT> {
+internal interface ViewStylist<ViewT : View, ItemT> {
 
     fun styleView(view: ViewT, item: ItemT, position: Int)
 }
