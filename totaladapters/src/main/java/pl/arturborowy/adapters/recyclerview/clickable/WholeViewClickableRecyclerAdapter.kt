ef710d2d95d82cd934092cdbf0075d0ff29dd76f
arturborowy <artur.borowy@proexe.pl>
2019-05-29 19:28:58 +0200

    Unify clickable recycler adapters with grid view adapters.

--- totaladapters/src/main/java/pl/arturborowy/adapters/recyclerview/clickable/WholeViewClickableRecyclerAdapter.kt
+++ totaladapters/src/main/java/pl/arturborowy/adapters/recyclerview/clickable/WholeViewClickableRecyclerAdapter.kt
@@ -1,11 +1,8 @@
 package pl.arturborowy.adapters.recyclerview.clickable
 
 import android.view.View
+import pl.arturborowy.adapters.common.WholeViewClickableAdapter
 
 abstract class WholeViewClickableRecyclerAdapter<ViewT : View, ItemT>(items: Collection<ItemT> =
                                                                               listOf()) :
-        ClickableViewRecyclerAdapter<ViewT, ItemT>(items) {
-
-    override fun styleView(view: ViewT, item: ItemT, position: Int) =
-            view.setOnClickListener { onItemClickAction(item, position) }
-}
+        ClickableViewRecyclerAdapter<ViewT, ItemT>(items), WholeViewClickableAdapter<ViewT, ItemT>
