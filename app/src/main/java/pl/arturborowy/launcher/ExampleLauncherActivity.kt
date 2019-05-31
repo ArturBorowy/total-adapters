57817917495956257eea0cf66193fc7d2881a2d3
arturborowy <artur.borowy@proexe.pl>
2019-05-31 18:41:54 +0200

    Create empty ExampleLauncherActivity.

---
+++ app/src/main/java/pl/arturborowy/launcher/ExampleLauncherActivity.kt
@@ -0,0 +1,13 @@
+package pl.arturborowy.launcher
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pl.arturborowy.R
+
+class ExampleLauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_launcher)
+    }
+}
