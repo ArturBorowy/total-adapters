6ccccae7d4b77b02bd4b6c7faf03874f88d7ce87
arturborowy <artur.borowy@proexe.pl>
2019-05-31 19:07:49 +0200

    Add logic of showing example to ExampleLauncherActivity.

--- app/src/main/java/pl/arturborowy/launcher/ExampleLauncherActivity.kt
+++ app/src/main/java/pl/arturborowy/launcher/ExampleLauncherActivity.kt
@@ -2,12 +2,53 @@ package pl.arturborowy.launcher
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_example_launcher.*
+import org.jetbrains.anko.startActivity
 import pl.arturborowy.R
+import pl.arturborowy.examples.recyclerview.base.BaseRecyclerAdapterExampleActivity
 
 class ExampleLauncherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_example_launcher)
+
+        showExampleBtn.setOnClickListener { showExample() }
+    }
+
+    private fun showExample() {
+        val isRecyclerViewChecked = recyclerViewRadio.isChecked
+        val isGridViewChecked = gridViewRadio.isChecked
+
+        val isBaseChecked = baseAdapterRadio.isChecked
+        val isClickableViewChecked = clickableViewAdapterRadio.isChecked
+        val isWholeViewClickableChecked = wholeViewClickableAdapterRadio.isChecked
+
+        when {
+            isRecyclerViewChecked -> showRecyclerViewExample(isBaseChecked,
+                    isClickableViewChecked, isWholeViewClickableChecked)
+            isGridViewChecked -> showGridViewExample(isBaseChecked,
+                    isClickableViewChecked, isWholeViewClickableChecked)
+        }
+    }
+
+    private fun showRecyclerViewExample(isBaseChecked: Boolean,
+                                        isClickableViewChecked: Boolean,
+                                        isWholeViewClickableChecked: Boolean) {
+        when {
+            isBaseChecked -> startActivity<BaseRecyclerAdapterExampleActivity>()
+            isClickableViewChecked -> TODO()
+            isWholeViewClickableChecked -> TODO()
+        }
+    }
+
+    private fun showGridViewExample(isBaseChecked: Boolean,
+                                    isClickableViewChecked: Boolean,
+                                    isWholeViewClickableChecked: Boolean) {
+        when {
+            isBaseChecked -> TODO()
+            isClickableViewChecked -> TODO()
+            isWholeViewClickableChecked -> TODO()
+        }
     }
 }
